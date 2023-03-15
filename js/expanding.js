700f7a0c5ba68f587e037c1b0a52537e9b97552a
Pengsu Yang <111721018+pengsuyang@users.noreply.github.com>
2023-03-15 17:22:49 +0000

    Wed, Mar 15, 2023, 10:22 AM -07:00

--- js/expanding.js
+++ js/expanding.js
@@ -1,65 +1,21 @@
-body {
-    display: flex;
-    height: 100vh;
-    align-items: center;
-    justify-content: center;
-    margin: 0;
-    overflow: hidden;
-    background: pink;
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click' => {
+        removeActive()
+        panel.classList.add('active')
+    })
+})
+
+
+
+
+
+
+function removeActive(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
 }
 
-
-.container{
-    display: flex;
-    width: 90vw;
-}
-
-
-.panel{
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    height: 80vh;
-    margin: 10px;
-    flex: 1;
-    border-radius: 50px;
-    cursor: pointer;
-    position: relative;
-    color: white;
-    transition: all 0.5s ease-in;
-}
-
-
-.panel.active{
-    flex: 10;
-
-
-}
-
-
-.panel.active h3{
-    opacity: 1;
-    transition: opacity 0.25s ease-in 0.25s;
-}
-
-
-.panel h3{
-    font-size: 24px;
-    position: absolute;
-    bottom: 20px;
-    left:20px;
-    opacity: 0;
-}
-
-
-@media (max-width: 480px) {
-    .container {
-        width: 100vh;
-    }
-
-
-    .panel:nth-of-type(4)
-    .panel:nth-of-type(5){
-        display:none;
-    }
-}
