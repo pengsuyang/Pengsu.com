8125d6c09a0f4be4a90f8c3dcc094825d6cd121e
Pengsu Yang <111721018+pengsuyang@users.noreply.github.com>
2023-03-17 17:02:34 +0000

    Fri, Mar 17, 2023, 10:02 AM -07:00

--- js/sounds.js
+++ js/sounds.js
@@ -4,8 +4,8 @@ sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText = sound
-    btn.eventListener('click', () => {
-        stongsongs()
+    btn.addeventListener('click', () => {
+        stopSongs()
         document.getElementById(sound).play()
     })
     document.getElementById('buttons').appendChild(btn)
