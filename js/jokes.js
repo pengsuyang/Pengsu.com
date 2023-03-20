1af881bbced396d6a8a1be059a2fea09b524015c
Pengsu Yang <111721018+pengsuyang@users.noreply.github.com>
2023-03-20 17:02:15 +0000

    Mon, Mar 20, 2023, 10:02 AM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -1,2 +1,13 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+async fuction generateJoke() {
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json',},
+    })
+
+    const data = await res.json()
+    jokeEl.innerHTMl = data.joke
+}
