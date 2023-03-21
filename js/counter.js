13a6f5b04f6365c4e68de51c084c0b96d68adab6
Pengsu Yang <111721018+pengsuyang@users.noreply.github.com>
2023-03-21 17:04:09 +0000

    Tue, Mar 21, 2023, 10:04 AM -07:00

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,10 @@
 const counters= document.querySelectAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+        
+    }
+})
