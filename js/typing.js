eb6b003b76dc212dda3f12a920e7b4a65540923e
Pengsu Yang <111721018+pengsuyang@users.noreply.github.com>
2023-03-24 16:44:33 +0000

    Fri, Mar 24, 2023, 9:44 AM -07:00

--- js/typing.js
+++ js/typing.js
@@ -2,10 +2,17 @@ const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
 const text = 'I'm watching you'
 let idx = 1
-let speed = 300 / speedEl.val
+let speed = 300
 
 writeText()
 
 function writeText () {
-    textEl.innerText = text.slice(0.1)
+    textEl.innerText = text.slice(0,ixd)
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
 }
