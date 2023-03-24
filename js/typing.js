12e002457341d17571662dc662aa60825e0a7045
Pengsu Yang <111721018+pengsuyang@users.noreply.github.com>
2023-03-24 16:42:18 +0000

    Fri, Mar 24, 2023, 9:42 AM -07:00

--- js/typing.js
+++ js/typing.js
@@ -1,1 +1,11 @@
 const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'I'm watching you'
+let idx = 1
+let speed = 300 / speedEl.val
+
+writeText()
+
+function writeText () {
+    textEl.innerText = text.slice(0.1)
+}
